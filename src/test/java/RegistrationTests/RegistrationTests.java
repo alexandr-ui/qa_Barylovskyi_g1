d3e742f90cb167f6181c16356c4828dd267de501
Alexandr Ba <alexandrba@5-eadmins-MacBook-Pro.local>
2021-08-14 15:30:16 +0300

    Create MainTests and update WebElements libs

--- src/test/java/RegistrationTests/RegistrationTests.java
+++ src/test/java/RegistrationTests/RegistrationTests.java
@@ -1,62 +1,14 @@
 package RegistrationTests;
 
-
-
-import org.apache.log4j.Logger;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.openqa.selenium.support.ui.Select;
+import MainTests.MainTest;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import pages.MainPage;
 import pages.RegistrationPage;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
 
-public class RegistrationTests {
+public class RegistrationTests extends MainTest {
 
-    public WebDriver webDriver;
-    private Logger logger = Logger.getLogger(getClass());
-    String email = "2undumewepp-642617@yopmail.com";
-
-    public RegistrationPage registrationPage;
-    public MainPage mainPage;
-
-
-
-    @Before
-    public void setUp(){
-        //Setup browser
-        File chromeFF = new File("./drivers/chromedriver");
-        System.setProperty("webdriver.chrome.driver", chromeFF.getAbsolutePath());
-        webDriver = new ChromeDriver();
-        registrationPage = new RegistrationPage(webDriver);
-        mainPage = new MainPage(webDriver);
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        try{
-            webDriver.get("http://automationpractice.com");
-            logger.info("Open URL - http://automationpractice.com");
-        } catch(Exception e){
-            logger.error("Can't open browser");
-            Assert.fail("Can't open browser");
-        }
-
-    }
-
-    @After
-    public void tearDown(){
-        //Close chromedriver
-        webDriver.manage().deleteAllCookies();
-//        logger.info("Clear cookies");
-//        webDriver.quit();
-//        logger.info("Close browser");
-    }
+    String email = "2undumewtepp-642617@yopmail.com";
 
     @Test
     public void testRegistrationValidByPageObj(){
@@ -86,6 +38,8 @@ public class RegistrationTests {
         registrationPage.clickSubmitAccount();
         registrationPage.titleRegisteredAccount.isDisplayed();
         logger.info("Verify if account created");
+        checkAC("Text is present",
+                registrationPage.titleRegisteredAccount.isDisplayed(), true);
 
 
 
